8a0c1c9f11d7c1386e96959ef539e2e5c78ca783
LocLinh <tomandjery4444@gmail.com>
2021-06-17 09:16:45 +0700

    ppt animation spin

---
+++ pptanimation_spin_counterclockwise.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('C:\\Users\\LocLinh\\Desktop\\CS321_ThiGiac\\saitama.png')
+
+width = img.shape[1]
+height = img.shape[0]
+channel = img.shape[2]
+
+for r in range(360):
+    image_center = tuple(np.array(img.shape[1::-1]) / 2)
+    rot_mat = cv2.getRotationMatrix2D(image_center, r, 1.0)
+    result = cv2.warpAffine(img, rot_mat, img.shape[1::-1], flags=cv2.INTER_LINEAR)
+    cv2.imshow('a', result)
+    cv2.waitKey(1)
+
+cv2.destroyAllWindows()
